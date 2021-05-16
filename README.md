b3ee3c0ae9941c2cc9d7d6e1e00e6b459d1990d8
hehelego <2364261262@qq.com>
2021-05-17 02:27:48 +0800

    upd(README, regen.py): update script to generate the site, and the readme file.

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # hehelego.github.io
-my gh pages site
+
+The github pages of [hehelego/spinach](https://github.com/hehelego/)
+
+- source: [github: hehelego/WhyNotMarkdown](https://github.com/hehelego/WhyNotMarkdown)
+- generator: [github: hehelego/WhyNotMarkdown/blog_builder.py](https://github.com/hehelego/WhyNotMarkdown/blob/master/backup/scripts/blog_builder.py)
+- **notice**: 
+  - Please send all the issues/pull-requests/merge-requests to [github: hehelego/WhyNotMarkdown](https://github.com/hehelego/WhyNotMarkdown).  
+  - This repo is only used as a host of github-pages.
+- content provided under **CC0 license** if no special declaration is made.
--- regen.py
+++ regen.py
@@ -1,9 +1,32 @@
 #!/usr/bin/python
 import subprocess
+import sys
 
-if __name__=='__main__':
+def fullyRegen():
     subprocess.run(['rm','-rf','WhyNotMarkdown/'])
     subprocess.run(['rm','-rf','converted/'])
     subprocess.run(['git','clone','https://github.com/hehelego/WhyNotMarkdown','--depth','1'])
     subprocess.run(['python','WhyNotMarkdown/backup/scripts/blog_builder.py'])
     subprocess.run(['rm','-rf','WhyNotMarkdown/'])
+def partiallyRegen():
+    subprocess.run(['rm','-rf','converted/'])
+    subprocess.run(['python','WhyNotMarkdown/backup/scripts/blog_builder.py'])
+
+def main():
+    if len(sys.argv)<2:
+        print('usage: ./regen.py (f|p)')
+        return
+
+    opt = sys.argv[1]
+    if opt=='f':
+        fullyRegen()
+    elif opt=='p':
+        partiallyRegen()
+    else:
+        print('usage: ./regen.py (f|p)')
+
+
+
+
+if __name__=='__main__':
+    main()
